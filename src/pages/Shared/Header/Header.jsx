a3b999704af81445fcc5baa868756ce79c6f6da4
abuabdullah23 <abuobaidazayed@gmail.com>
2023-05-21 07:58:23 +0600

    Current Time setup

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../../../assets/logo.png';
+import moment from 'moment';
 
 const Header = () => {
     return (
@@ -7,6 +8,7 @@ const Header = () => {
             <div className="text-center">
                 <img src={logo} alt="" />
                 <p className='mt-3 text-secondary'>Journalism Without Fear or Favor</p>
+                <p>{moment().format("dddd, MMMM D, YYYY")}</p>
             </div>
         </div>
     );
