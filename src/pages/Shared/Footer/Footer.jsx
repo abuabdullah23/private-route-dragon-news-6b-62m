4493b13a670daae93a8ce7b9b6a672e21bf3d1e1
abuabdullah23 <abuobaidazayed@gmail.com>
2023-05-21 07:11:08 +0600

    Route setup for large site

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>This is my copyright 2023</small></p>
+        </div>
+    );
+};
+
+export default Footer;
